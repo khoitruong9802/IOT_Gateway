095ff9258f325b6bb7f8e4d4a53d784e479a18de
khoitruong9802 <khoitruong9802@gmail.com>
2024-12-10 22:02:37 +0700

    Update gateway

--- scheduler/core.py
+++ scheduler/core.py
@@ -22,67 +22,68 @@ class Core:
   
     if instruction.get("instr") == "watering":
       watering_args = instruction.get("args").split(" ")
-      fertilizer = Fertilizer()
-      fertilizer.control_relay(1, True)
-      sleep(int(watering_args[0]) / 5)
-      fertilizer.control_relay(1, False)
 
-      fertilizer.control_relay(2, True)
-      sleep(int(watering_args[1]) / 5)
-      fertilizer.control_relay(2, False)
-
-      fertilizer.control_relay(3, True)
-      sleep(int(watering_args[2]) / 5)
-      fertilizer.control_relay(3, False)
-
-      fertilizer.control_relay(7, True)
-      sleep(5)
-      fertilizer.control_relay(7, False)
-
-      fertilizer.control_relay(int(watering_args[3]) + 3, True)
-      sleep(0.5)
-
-      fertilizer.control_relay(8, True)
-      sleep(5)
-      fertilizer.control_relay(8, False)
-
-      sleep(0.5)
-      fertilizer.control_relay(int(watering_args[3]) + 3, False)
-
-      # print(f"flow 1 = {watering_args[0]}")
-      # print("Relay 1 on")
+      # fertilizer = Fertilizer()
+      # fertilizer.control_relay(1, True)
       # sleep(int(watering_args[0]) / 5)
-      # print("Relay 1 off")
+      # fertilizer.control_relay(1, False)
 
-      # print(f"flow 2 = {watering_args[1]}")
-      # print("Relay 2 on")
+      # fertilizer.control_relay(2, True)
       # sleep(int(watering_args[1]) / 5)
-      # print("Relay 2 off")
+      # fertilizer.control_relay(2, False)
 
-      # print(f"flow 3 = {watering_args[2]}")
-      # print("Relay 3 on")
+      # fertilizer.control_relay(3, True)
       # sleep(int(watering_args[2]) / 5)
-      # print("Relay 3 off")
+      # fertilizer.control_relay(3, False)
 
-      # print("Pump in")
-      # print("Relay 7 on")
+      # fertilizer.control_relay(7, True)
       # sleep(5)
-      # print("Relay 7 off")
+      # fertilizer.control_relay(7, False)
 
-      # print(f"Open water valve area {int(watering_args[3]) + 3}")
-      # print(f"Relay {int(watering_args[3]) + 3} on")
+      # fertilizer.control_relay(int(watering_args[3]) + 3, True)
       # sleep(0.5)
 
-      # print("Pump out")
-      # print("Relay 8 on")
+      # fertilizer.control_relay(8, True)
       # sleep(5)
-      # print("Relay 8 off")
+      # fertilizer.control_relay(8, False)
 
-      # print(f"Close water valve area {int(watering_args[3]) + 3}")
-      # print(f"Relay {int(watering_args[3]) + 3} off")
       # sleep(0.5)
+      # fertilizer.control_relay(int(watering_args[3]) + 3, False)
+
+      print(f"flow 1 = {watering_args[0]}")
+      print("Relay 1 on")
+      sleep(int(watering_args[0]) / 5)
+      print("Relay 1 off")
+
+      print(f"flow 2 = {watering_args[1]}")
+      print("Relay 2 on")
+      sleep(int(watering_args[1]) / 5)
+      print("Relay 2 off")
+
+      print(f"flow 3 = {watering_args[2]}")
+      print("Relay 3 on")
+      sleep(int(watering_args[2]) / 5)
+      print("Relay 3 off")
+
+      print("Pump in")
+      print("Relay 7 on")
+      sleep(5)
+      print("Relay 7 off")
+
+      print(f"Open water valve area {int(watering_args[3]) + 3}")
+      print(f"Relay {int(watering_args[3]) + 3} on")
+      sleep(0.5)
+
+      print("Pump out")
+      print("Relay 8 on")
+      sleep(5)
+      print("Relay 8 off")
+
+      print(f"Close water valve area {int(watering_args[3]) + 3}")
+      print(f"Relay {int(watering_args[3]) + 3} off")
+      sleep(0.5)
    
-      # print("end!!!!!!!!")
+      print("end!!!!!!!!")
 
     self.core_state = CoreState.IDLE
     return task_PCB
